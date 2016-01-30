ccca57b6397aa73a074352578690f674b06f04e5
Jerry Yuan <d.jerryuan@gmail.com>
2016-01-30 12:17:13 -0600

    attempt at bullet logic

---
+++ Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletLogic : MonoBehaviour {
+
+    private float maxSpeed;
+    private float damage;
+    private Vector2 direction;
+
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        maxSpeed = 250;
+        rb2D = GetComponent<Rigidbody2D>();
+        direction = PlayerLogic.getDirection();
+        rb2D.AddRelativeForce(PlayerLogic.getDirection() * maxSpeed);
+    }
+
+    void FixedUpdate()
+    {
+
+        // On collision stuff
+
+    }
+}
--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class PlayerLogic : MonoBehaviour {
 
-	private float speed;
-	private float maxSpeed;
+    private float speed;
+    private float maxSpeed;
+
+    private Vector2 direction;
+    public Vector2 getDirection()
+    {
+        return direction;
+    }
+    public void setDirection(Vector2 dir)
+    {
+        direction = dir;
+    }
 
 	private Rigidbody2D rb2D;
 
@@ -12,6 +22,7 @@ public class PlayerLogic : MonoBehaviour {
 		speed = 10;
 		maxSpeed = 5;
 		rb2D = GetComponent<Rigidbody2D>();
+        direction = Vector2.right;
 	}
 
 	void FixedUpdate()
@@ -22,8 +33,21 @@ public class PlayerLogic : MonoBehaviour {
 		rb2D.AddRelativeForce (Vector2.right * inputX * speed);
 		rb2D.AddRelativeForce (Vector2.up * inputY * speed);
 
+        // store rotation into direction vector
+
 		if (rb2D.velocity.magnitude > maxSpeed) {
 			rb2D.velocity = rb2D.velocity.normalized * maxSpeed;
 		}
+
+        // while (Input.GetKey("space"))
+        //     spawnBulletA();
+            
 	}
+
+    //
+    private void spawnBulletA()
+    {
+
+        Instantiate(enemyA, new Vector3(xSpawn + config.getLeftBoundary(), ySpawn + config.getBottomBoundary(), 0), Quaternion.identity);
+    }
 }
